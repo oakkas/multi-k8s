fed66e16c21d67b78b9d381d0dba703220fd0534
oakkas <akkaso@b20332.local>
2019-12-26 12:04:01 -0700

    edited deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
-docker build oakkas/multi-client:latest -t oakkas/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build oakkas/multi-server:latest -t oakkas/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build oakkas/multi-worker:latest -t oakkas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t oakkas/multi-client:latest -t oakkas/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t oakkas/multi-server:latest -t oakkas/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t oakkas/multi-worker:latest -t oakkas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push oakkas/multi-client:latest
-docker push oakkas/multi-client:$SHA
-
 docker push oakkas/multi-server:latest
-docker push oakkas/multi-server:$SHA
-
 docker push oakkas/multi-worker:latest
+
+docker push oakkas/multi-client:$SHA
+docker push oakkas/multi-server:$SHA
 docker push oakkas/multi-worker:$SHA
 
 kubectl apply -f k8s
