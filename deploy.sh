27b086da5a03f19dc16597b128a0cfa338b00030
oakkas <akkaso@b20332.local>
2019-12-26 11:57:10 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build oakkas/multi-client:latest -t oakkas/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build oakkas/multi-server:latest -t oakkas/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build oakkas/multi-worker:latest -t oakkas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push oakkas/multi-client:latest
+docker push oakkas/multi-client:$SHA
+
+docker push oakkas/multi-server:latest
+docker push oakkas/multi-server:$SHA
+
+docker push oakkas/multi-worker:latest
+docker push oakkas/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=oakkas/multi-server:$SHA
+kubectl set image deployments/client-deployment client=oakkas/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=oakkas/multi-worker:$SHA
